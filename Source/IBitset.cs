3d08f74feff3bce8446d113a28449849d82540dc
mamun-tanveer <mamun.tanveer@outlook.com>
2015-11-14 21:05:32 -0600

    Initial Project setup
    Created a .csproj and the initial IBitset interface.

---
+++ Source/IBitset.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source
+{
+    public interface IBitset 
+    {
+        IBitset And(IBitset x);
+        IBitset Or(IBitset y);
+        IBitset Length();
+        
+    }
+}
