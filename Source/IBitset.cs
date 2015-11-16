d8a9b03efa70a5109fb580331701acecdddbc308
mamun-tanveer <mamun.tanveer@outlook.com>
2015-11-15 20:30:35 -0600

    Basic interface adds and cleanup
    Added setter methods on individual bits and IsOne. Got rid of
    unnecessary using statements. Fixed namespace.

--- Source/IBitset.cs
+++ Source/IBitset.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Source
+﻿namespace BitsetsNET
 {
+    /// <summary>
+    /// This is the common interface for all bitsets regardless of compression. It defines common operations. 
+    /// </summary>
     public interface IBitset 
     {
         IBitset And(IBitset x);
         IBitset Or(IBitset y);
         IBitset Length();
-        
+        void SetOne(int index);
+        void SetZero(int index);
+        bool IsOne(int index);
     }
 }
