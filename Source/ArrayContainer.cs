7144309111713a04e68f62b81224bb850e8a11c4
Julianna Peebles <j.d.peebles@gmail.com>
2015-11-20 16:20:44 -0600

    Add properties + constructors for container classes

--- Source/ArrayContainer.cs
+++ Source/ArrayContainer.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BitsetsNET
 {
-    class ArrayContainer
+    class ArrayContainer : Container
     {
+        private const int DEFAULT_INIT_SIZE = 4;
+        private const int DEFAULT_MAX_SIZE = 4096;
+
+        protected int cardinality;
+        protected short[] content;
+
+        // TODO: FIX THIS
+        //public ArrayContainer()
+        //{
+        //    this(DEFAULT_INIT_SIZE); //I'm not sure what the C# equivalent of this is
+        //}
+        
+        public ArrayContainer(int capacity)
+        {
+            this.cardinality = 0;
+            this.content = new short[capacity];
+        }
+
     }
 }
