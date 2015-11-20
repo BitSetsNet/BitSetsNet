42272e3d9d94dc4c0351eeaa5b46d87aa523fa53
mamun-tanveer <mamun.tanveer@outlook.com>
2015-11-20 11:21:54 -0600

    Actually passing tests
    Basic testing passing for UncompressedBitSets

--- Tests/BaseBitSetTests.cs
+++ Tests/BaseBitSetTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Linq;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,57 +12,86 @@ namespace BitsetsNET.Tests
     [TestClass]
     public abstract class BaseBitSetTests
     {
-        protected BitsetsNET.IBitset TestSet { get; set; }
-        protected BitsetsNET.IBitset TestSet2 { get; set; }
-        protected BitsetsNET.IBitset AndResult { get; set; }
         protected BitsetsNET.IBitset OrResult { get; set; }
         protected KeyValuePair<int, bool> GetResult { get; set; }
-        protected int ExpectedLength = 0; 
+        protected int ExpectedLength = 0;
+
+        protected abstract IBitset CreateSetFromIndicies(int[] indices, int length);
 
         [TestMethod()]
         public virtual void AndTest()
         {
-            Assert.AreEqual(AndResult, TestSet.And(TestSet2));
+            int[] first = { 1, 2, 3 };
+            int[] second = { 1, 4, 5 };
+            int[] result = first.Intersect(second).ToArray();
+            IBitset expected = CreateSetFromIndicies(result, 10);
+            IBitset actual = CreateSetFromIndicies(first, 10).And(CreateSetFromIndicies(second, 10));
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
         public virtual void AndWithTest()
         {
-            TestSet.AndWith(TestSet2);
-            Assert.AreEqual(AndResult, TestSet);
+            int[] first = { 1, 2, 3 };
+            int[] second = { 1, 4, 5 };
+            int[] result = first.Intersect(second).ToArray();
+            IBitset testSet = CreateSetFromIndicies(first, 10);
+            testSet.AndWith(CreateSetFromIndicies(second, 10));
+
+            Assert.AreEqual(CreateSetFromIndicies(result, 10), testSet);
+  
         }
 
         [TestMethod()]
         public virtual void CloneTest()
         {
-            var clone = TestSet.Clone();
-            Assert.AreEqual(clone, TestSet);
+            int[] set = { 1, 5, 7 };
+            IBitset testSet = CreateSetFromIndicies(set, 10);
+            var clone = testSet.Clone();
+            Assert.AreEqual(clone, testSet);
         }
 
         [TestMethod()]
         public virtual void GetTest()
         {
-            bool result = TestSet.Get(GetResult.Key);
-            Assert.AreEqual(result, GetResult.Value);
+            int[] set = { 2, 7, 9 };
+            IBitset testSet = CreateSetFromIndicies(set, 10);
+            bool expected = true;
+            bool result = testSet.Get(2);
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod()]
         public virtual void LengthTest()
         {
-            Assert.AreEqual(ExpectedLength, TestSet.Length());
+            int[] set = { 1 };
+            IBitset testSet = CreateSetFromIndicies(set, 10);
+            Assert.AreEqual(10, testSet.Length());
         }
 
         [TestMethod()]
         public virtual void OrTest()
         {
-            Assert.AreEqual(TestSet, TestSet2);
+            int[] first = { 1, 2, 3 };
+            int[] second = { 1, 4, 5 };
+            int[] result = first.Union(second).ToArray();
+            IBitset expected = CreateSetFromIndicies(result, 10);
+            IBitset actual = CreateSetFromIndicies(first, 10).Or(CreateSetFromIndicies(second, 10));
+
+            Assert.AreEqual(expected, actual);
 
         }
 
         [TestMethod()]
         public virtual void OrWithTest()
         {
+            int[] first = { 1, 2, 3 };
+            int[] second = { 1, 4, 5 };
+            int[] result = first.Union(second).ToArray();
+            IBitset testSet = CreateSetFromIndicies(first, 10);
+            testSet.OrWith(CreateSetFromIndicies(second, 10));
 
+            Assert.AreEqual(CreateSetFromIndicies(result, 10), testSet);
         }
 
         [TestMethod()]
