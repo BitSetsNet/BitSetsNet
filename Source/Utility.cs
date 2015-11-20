654f427899a5c249d6f92d9fa1c0f3a39473b3c5
Tori Kraft <tori.kraft@mcmaster.com>
2015-11-20 17:12:50 -0600

    Finish out branching binary search

--- Source/Utility.cs
+++ Source/Utility.cs
@@ -25,13 +25,35 @@ namespace BitsetsNET
         }
 
         public static int unsignedBinarySearch(short[] array, int begin, int end, short k) {
-       
-            //optimizes for the case where the value is inserted at the end
-            //if ((end > 0) && (toIntUnsigned(array[end - 1]) < ikey))
-            //{
 
-            //}
+            int ikey = toIntUnsigned(k);
+            //optimizes for the case where the value is inserted at the end
+            if ((end > 0) && (toIntUnsigned(array[end - 1]) < ikey))
+            {
+                return -end - 1;
+            }
+            int low = begin;
+            int high = end - 1;
+            while (low <= high) {
+
+                //convert to uint to shift unsigned by one, then convert back
+                int middleIndex = (int)((uint)(low + high) >> 1);
+                int middleValue = toIntUnsigned(array[middleIndex]);
+
+                if (middleIndex < ikey) {
+                    low = middleIndex + 1;
+                } else if (middleValue > ikey) {
+                    high = middleIndex - 1;
+                } else {
+                    return middleIndex;
+                }
+                return -(low + 1);
+            }
             return 0;
+        }
+
+        protected static int toIntUnsigned(short x) {
+            return x & 0xFFFF;
         }
 
     }
