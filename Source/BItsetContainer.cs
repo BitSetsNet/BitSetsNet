4efd714553c8955bacf8d00a96024c8093c3bae0
Julianna Peebles <j.d.peebles@gmail.com>
2015-11-20 16:20:44 -0600

    Add properties + constructors for container classes

--- Source/BItsetContainer.cs
+++ Source/BItsetContainer.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace BitsetsNET
 {
-    class BitsetContainer
+    class BitsetContainer : Container
     {
+        protected const int MAX_CAPACITY = 1 << 64;
+
+        int cardinality;
+        long[] bitmap; //Should we use a BitArray object?
+
+        public BitsetContainer()
+        {
+            this.cardinality = 0;
+            this.bitmap = new long[MAX_CAPACITY / 64];
+        }
+
     }
 }
