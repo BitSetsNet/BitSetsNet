5d500fedd9473ceab76bc7deb926973c60b6a397
Wale Hassan <wale.hassan@mcmaster.com>
2015-11-21 16:14:52 -0600

    implemented Roaring Bitset And

--- Source/RoaringArray.cs
+++ Source/RoaringArray.cs
@@ -21,6 +21,66 @@ namespace BitsetsNET
             values = new Container[INITIAL_CAPACITY];
         }
 
+        public void append(ushort key, Container value)
+        {
+            extendArray(1);
+            this.keys[this.size] = key;
+            this.values[this.size] = value;
+            this.size++;
+        }
+
+        public int advanceUntil(ushort x, int pos)
+        {
+            int lower = pos + 1;
+
+            // special handling for a possibly common sequential case
+            if (lower >= size || keys[lower] >= x)
+            {
+                return lower;
+            }
+
+            int spansize = 1; // could set larger
+            // bootstrap an upper limit
+
+            while (lower + spansize < size && keys[lower + spansize] < x)
+                spansize *= 2; // hoping for compiler will reduce to shift
+            int upper = (lower + spansize < size) ? lower + spansize : size - 1;
+
+            // maybe we are lucky (could be common case when the seek ahead
+            // expected to be small and sequential will otherwise make us look bad)
+            if (keys[upper] == x)
+            {
+                return upper;
+            }
+
+            if (keys[upper] < x)
+            {// means array has no item key >= x
+                return size;
+            }
+
+            // we know that the next-smallest span was too small
+            lower += (spansize / 2);
+
+            // else begin binary search
+            // invariant: array[lower]<x && array[upper]>x
+            while (lower + 1 != upper)
+            {
+                int mid = (lower + upper) / 2;
+                if (keys[mid] == x)
+                    return mid;
+                else if (keys[mid] < x)
+                    lower = mid;
+                else
+                    upper = mid;
+            }
+            return upper;
+        }
+
+        public ushort getKeyAtIndex(int i)
+        {
+            return this.keys[i];
+        }
+
         public int getIndex(ushort x)
         {
             //TODO: optimize this
