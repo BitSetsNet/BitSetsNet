5e5619f1af6be7c5a667a98c2036c694865dc1e9
Wale Hassan <wale.hassan@mcmaster.com>
2015-11-21 15:40:20 -0600

    implemented And for array containers

--- Source/Utility.cs
+++ Source/Utility.cs
@@ -100,5 +100,195 @@ namespace BitsetsNET
                 }
             }
         }
+
+        /**
+         * Intersect two sorted lists and write the result to the provided
+         * output array
+         *
+         * @param set1    first array
+         * @param length1 length of first array
+         * @param set2    second array
+         * @param length2 length of second array
+         * @param buffer  output array
+         * @return cardinality of the intersection
+         */
+        public static int unsignedIntersect2by2(ushort[] set1,
+                                                int length1, ushort[] set2, int length2,
+                                                ushort[] buffer) {
+            if (set1.Length * 64 < set2.Length) {
+                return unsignedOneSidedGallopingIntersect2by2(set1, length1, set2, length2, buffer);
+            } else if (set2.Length * 64 < set1.Length) {
+                return unsignedOneSidedGallopingIntersect2by2(set2, length2, set1, length1, buffer);
+            } else {
+                return unsignedLocalIntersect2by2(set1, length1, set2, length2, buffer);
+            }
+        }
+
+        protected static int unsignedLocalIntersect2by2(ushort[] set1,
+                                                        int length1, ushort[] set2, int length2,
+                                                        ushort[] buffer) {
+            if ((0 == length1) || (0 == length2))
+                return 0;
+            int k1 = 0;
+            int k2 = 0;
+            int pos = 0;
+            ushort s1 = set1[k1];
+            ushort s2 = set2[k2];
+
+            bool breakflag = false;
+            while (!breakflag) {
+                int v1 = s1;
+                int v2 = s2;
+                if (v2 < v1) {
+                    do {
+                        ++k2;
+                        if (k2 == length2)
+                        {
+                            breakflag = false;
+                            break;
+                        }
+
+                        s2 = set2[k2];
+                        v2 = s2;
+                    } while (v2 < v1);
+                }
+                if (v1 < v2) {
+                    do {
+                        ++k1;
+                        if (k1 == length1)
+                        {
+                            breakflag = false;
+                            break;
+                        }
+
+                        s1 = set1[k1];
+                        v1 = s1;
+                    } while (v1 < v2);
+                } else {
+                    // (set2[k2] == set1[k1])
+                    buffer[pos++] = s1;
+                    ++k1;
+                    if (k1 == length1)
+                        break;
+                    ++k2;
+                    if (k2 == length2)
+                        break;
+                    s1 = set1[k1];
+                    s2 = set2[k2];
+                }
+            }
+            return pos;
+        }
+
+        protected static int unsignedOneSidedGallopingIntersect2by2(
+                ushort[] smallSet, int smallLength,
+                ushort[] largeSet, int largeLength,
+                ushort[] buffer) {
+            if (0 == smallLength)
+                return 0;
+            
+            int k1 = 0;
+            int k2 = 0;
+            int pos = 0;
+            ushort s1 = largeSet[k1];
+            ushort s2 = smallSet[k2];
+
+            while (true) {
+                if (s1 < s2) {
+                    k1 = advanceUntil(largeSet, k1, largeLength, s2);
+                    if (k1 == largeLength)
+                        break;
+                    s1 = largeSet[k1];
+                }
+                if (s2 < s1) {
+                    ++k2;
+                    if (k2 == smallLength)
+                        break;
+                    s2 = smallSet[k2];
+                } else {
+                    // (set2[k2] == set1[k1])
+                    buffer[pos++] = s2;
+                    ++k2;
+                    if (k2 == smallLength)
+                        break;
+                    s2 = smallSet[k2];
+                    k1 = advanceUntil(largeSet, k1, largeLength, s2);
+                    if (k1 == largeLength)
+                        break;
+                    s1 = largeSet[k1];
+                }
+
+            }
+            return pos;
+
+        }
+
+        /// <summary>
+        /// Find the smallest integer larger than pos such that array[pos] >= min.
+        /// If none can be found, return length. Based on code by O. Kaser.
+        /// </summary>
+        /// <param name="array">array to search within</param>
+        /// <param name="pos">starting position of the search</param>
+        /// <param name="length">length of the array to search</param>
+        /// <param name="min">minimum value</param>
+        /// <returns>x greater than pos such that array[pos] is at least as large
+        /// as min, pos is is equal to length if it is not possible.</returns>
+        public static int advanceUntil(ushort[] array, int pos, int length, ushort min)
+        {
+            int lower = pos + 1;
+
+            // special handling for a possibly common sequential case
+            if (lower >= length || array[lower] >= min)
+            {
+                return lower;
+            }
+
+            int spansize = 1; // could set larger
+            // bootstrap an upper limit
+
+            while (lower + spansize < length && toIntUnsigned(array[lower + spansize]) < toIntUnsigned(min))
+                spansize *= 2; // hoping for compiler will reduce to
+            // shift
+            int upper = (lower + spansize < length) ? lower + spansize : length - 1;
+
+            // maybe we are lucky (could be common case when the seek ahead
+            // expected
+            // to be small and sequential will otherwise make us look bad)
+            if (array[upper] == min)
+            {
+                return upper;
+            }
+
+            if (toIntUnsigned(array[upper]) < toIntUnsigned(min))
+            {// means
+                // array
+                // has no
+                // item
+                // >= min
+                // pos = array.length;
+                return length;
+            }
+
+            // we know that the next-smallest span was too small
+            lower += (spansize / 2);
+
+            // else begin binary search
+            // invariant: array[lower]<min && array[upper]>min
+            while (lower + 1 != upper)
+            {
+                int mid = (lower + upper) / 2;
+                ushort arraymid = array[mid];
+                if (arraymid == min)
+                {
+                    return mid;
+                }
+                else if (arraymid < min)
+                    lower = mid;
+                else
+                    upper = mid;
+            }
+            return upper;
+
+        }
     }
 }
