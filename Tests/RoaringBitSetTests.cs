d5e8e0d51e56ecdac237ebfcb5dad26471e8f3d5
Wale Hassan <wale.hassan@mcmaster.com>
2015-11-21 21:06:45 -0600

    begin implementing and testing IBitSet for RoaringBitset

---
+++ Tests/RoaringBitSetTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsetsNET.Tests
+{
+    [TestClass()]
+    public class RoaringBitSetTests : BaseBitSetTests 
+    {
+        protected override IBitset CreateSetFromIndicies(int[] indices, int length)
+        {
+            return RoaringBitset.Crte(indices);
+        }
+    }
+}
