74b280e642e26e5b648fd44827e248d7910bb1a4
Jared <jared.vonhalle@gmail.com>
2015-11-24 02:40:05 -0600

    interface for container. now compiles

--- Source/ArrayContainer.cs
+++ Source/ArrayContainer.cs
@@ -6,25 +6,91 @@ using System.Threading.Tasks;
 
 namespace BitsetsNET
 {
-    class ArrayContainer : Container
+    class ArrayContainer  :Container
     {
-        private const int DEFAULT_INIT_SIZE = 4;
-        private const int DEFAULT_MAX_SIZE = 4096;
-
-        protected int cardinality;
-        protected short[] content;
-
-        // TODO: FIX THIS
-        //public ArrayContainer()
-        //{
-        //    this(DEFAULT_INIT_SIZE); //I'm not sure what the C# equivalent of this is
-        //}
-        
-        public ArrayContainer(int capacity)
+        public override Container add(short x)
         {
-            this.cardinality = 0;
-            this.content = new short[capacity];
+            return new ArrayContainer();
         }
 
+        public override Container and(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container and(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool contains(short x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void fillLeastSignificant16bits(int[] x, int i, int mask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int getCardinality()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container iand(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container iand(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool intersects(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool intersects(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container ior(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container ior(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container or(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container or(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container remove(short x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override short select(int j)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Source/BItsetContainer.cs
+++ Source/BItsetContainer.cs
@@ -8,15 +8,89 @@ namespace BitsetsNET
 {
     class BitsetContainer : Container
     {
-        protected const int MAX_CAPACITY = 1 << 64;
-
-        int cardinality;
-        long[] bitmap; //Should we use a BitArray object?
-
-        public BitsetContainer()
+        public override Container add(short x)
         {
-            this.cardinality = 0;
-            this.bitmap = new long[MAX_CAPACITY / 64];
+            throw new NotImplementedException();
+        }
+
+        public override Container and(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container and(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool contains(short x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void fillLeastSignificant16bits(int[] x, int i, int mask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int getCardinality()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container iand(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container iand(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool intersects(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool intersects(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container ior(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container ior(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container or(BitsetContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container or(ArrayContainer x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Container remove(short x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override short select(int j)
+        {
+            throw new NotImplementedException();
         }
 
     }
