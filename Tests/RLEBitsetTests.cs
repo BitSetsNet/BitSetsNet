d4c25db4f833b4174d43ae50f1c6c98be39a2dc1
mamun <mamun.tanveer@mcmaster.com>
2015-12-02 15:57:09 -0600

    Added test class.

--- Tests/RLEBitsetTests.cs
+++ Tests/RLEBitsetTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace BitsetsNET.Tests
 {
-    class RLEBitsetTests : BaseBitSetTests
+    [TestClass()]
+    public class RLEBitsetTests : BaseBitSetTests
     {
         protected override IBitset CreateSetFromIndicies(int[] indices, int length)
         {
