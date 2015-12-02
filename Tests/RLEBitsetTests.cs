340d256877069b50cd9459d6c0972b5cedf9a746
mamun <mamun.tanveer@mcmaster.com>
2015-12-02 15:54:07 -0600

    Added Tests for RLE

---
+++ Tests/RLEBitsetTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsetsNET.Tests
+{
+    class RLEBitsetTests : BaseBitSetTests
+    {
+        protected override IBitset CreateSetFromIndicies(int[] indices, int length)
+        {
+            return RLE.RLEBitset.CreateFrom(indices);
+        }
+    }
+}
